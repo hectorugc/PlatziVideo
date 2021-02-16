7fc273a3325b4a657fa2ec30b6d62c4b69c6a1ba
hectorugc <61039458+hectorugc@users.noreply.github.com>
2021-02-16 21:17:29 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Button from "../src/components/Button";
+export default function App() {
+  return (
+    <div className="App">
+      <Button />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
